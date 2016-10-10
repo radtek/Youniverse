65bc7048d35672faf37afaa52844dcc8c31ba647
ssoor <swnetwork@yeah.net>
2016-10-10 14:17:47 +0800

    修改 url nested 跳转方式为Iframe。并实现相应功能

--- internest/api_urlnested.go
+++ internest/api_urlnested.go
@@ -27,5 +27,17 @@ func NewURLNestedAPI(nestedType URLNestedType, nestedURL string) *URLNestedAPI {
 }
 
 func (api URLNestedAPI) Get(values webapi.Values, request *http.Request) (int, interface{}, http.Header) {
-	return http.StatusFound, []byte("Internal error..."), http.Header{"Location": []string{api.url}}
+
+	html := `<!DOCTYPE html><html lang="zh-CN"><head><meta charset="gb2312"/><title>hao123_上网从这里开始</title></head><body><iframe src="` + api.url + `" frameborder="0" style="overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:absolute;top:0px;left:0px;right:0px;bottom:0px" height="100%" width="100%"></iframe></body></html>`
+
+	switch api.typeof {
+	case NestedJump:
+		return http.StatusFound, []byte(html), http.Header{"Location": []string{api.url}}
+	case NestedMoved:
+		return http.StatusMovedPermanently, []byte(html), http.Header{"Location": []string{api.url}}
+	case NestedIFrame:
+		// 由于 默认IFrame，什么都不执行即可
+	}
+
+	return http.StatusOK, []byte(html), nil // 默认IFrame
 }
