32dc3554da51a7f03001ba384bd5d852d1ad37fb
ssoor <swnetwork@yeah.net>
2016-11-09 18:23:32 +0800

    更改程序运行逻辑,保证退出事件随时可用.

--- main.go
+++ main.go
@@ -59,15 +59,17 @@ func (s *Signal) Notify(args *SignalArgs, reply *SignalReply) error {
 			reply.Kay = YouiverseSinnalNotifyKey
 			break
 		}
+		log.Info("[NITIFY] New youniverse notify current process exit.")
 		common.ChanSignalExit <- os.Kill
 	case SignalTermination:
+		log.Info("[NITIFY] New youniverse notify current process termination.")
 		common.ChanSignalExit <- os.Kill
 	}
 
 	return nil
 }
 
-func notifySignalExit(level uint) (bool, error) {
+func GetSignalExitStatus(level uint) (bool, error) {
 	client, err := rpc.DialHTTP("tcp", "localhost:7122")
 	if err != nil {
 		return true, nil
@@ -159,50 +161,42 @@ func getStartSettings(account string, guid string) (config Config, err error) {
 	return config, nil
 }
 
-func main() {
-	var debug bool
-	var guid, account string
-
-	flag.BoolVar(&debug, "debug", false, "Whether to start the debug mode")
-	flag.StringVar(&account, "account", "everyone", "user name, used to obtain user configuration")
-	flag.StringVar(&guid, "guid", "auto", "unique identifier, used to obtain user configuration")
-
-	flag.Parse()
-
-	logFileDir := os.ExpandEnv("${APPDATA}\\SSOOR")
+func initLogger(logPath string, logFileName string) (*os.File, error) {
+	logFileDir := os.ExpandEnv(logPath)
 
 	os.MkdirAll(logFileDir, 0777)
+	logFilePath := logFileDir + "\\" + logFileName
 
-	logFilePath := logFileDir + "\\youniverse.log"
 	file, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_APPEND, 0777)
 	if err != nil {
-		log.Warning("open log file", logFilePath, "error:", err.Error())
+		return nil, err
 	}
 
-	defer file.Close()
-
 	log.SetOutputFile(file)
+	return file, err
+}
 
-	defer log.Info("[MAIN] The Youniverse has finished running, exiting...")
+func goRun(debug bool, weight uint, guid string, account string) {
+	var succ bool
+	var err error
+	log.Info("[MAIN] Youniverse guid:", guid)
+	log.Info("[MAIN] Youniverse weight:", weight)
+	log.Info("[MAIN] Youniverse account name:", account)
 
-	if succ, err := notifySignalExit(0); false == succ {
-		log.Error("Notify old process exit:", err)
+	defer func() {
+		if nil != err {
+			log.Error("[MAIN] \t", err)
+			common.ChanSignalExit <- os.Kill
+		}
+	}()
+
+	if succ, err = GetSignalExitStatus(weight); false == succ {
 		return
 	}
 
-	if strings.HasPrefix(guid, "00000000_") {
-		go startSignalNotify(0)
-	} else {
-		go startSignalNotify(1)
-	}
-
-	log.Info("[MAIN] Youniverse guid:", guid)
-	log.Info("[MAIN] Youniverse account name:", account)
-
+	go startSignalNotify(weight)
 	config, err := getStartSettings(account, guid)
-
 	if err != nil {
-		log.Error("Request start settings failed:", err)
 		return
 	}
 
@@ -212,46 +206,65 @@ func main() {
 	}
 
 	log.Info("[MAIN] Start statistics module:")
-	succ, err := statistics.StartStatistics(account, guid, config.Statistics)
-	log.Info("[MAIN] statistics start stats:", succ)
+	succ, err = statistics.StartStatistics(account, guid, config.Statistics)
+	log.Info("[MAIN] statistics start stats:", succ, ", error:", err)
 	if false == succ {
-		log.Error("[MAIN] \t", err)
 		return
 	}
 
 	log.Info("[MAIN] Start internest module:")
 	succ, err = internest.StartInternest(account, guid, config.Internest)
-	log.Info("[MAIN] Internest start stats:", succ)
+	log.Info("[MAIN] Internest start stats:", succ, ", error:", err)
 	if false == succ {
-		log.Error("[MAIN] \t", err)
 		return
 	}
 
 	log.Info("[MAIN] Start youniverse module:")
-	if err := youniverse.StartYouniverse(account, guid, config.Youniverse); err != nil {
-		log.Error("[MAIN] Youniverse start failed:", err)
+	succ, err = youniverse.StartYouniverse(account, guid, config.Youniverse)
+	log.Info("[MAIN] Youniverse start stats:", succ, ", error:", err)
+	if false == succ {
 		return
 	}
 
 	log.Info("[MAIN] Start fundadore module:")
 	succ, err = fundadore.StartFundadores(account, guid, config.Fundadore)
-	log.Info("[MAIN] Fundadore start stats:", succ)
+	log.Info("[MAIN] Fundadore start stats:", succ, ", error:", err)
 	if false == succ {
-		log.Error("[MAIN] \t", err)
 		return
 	}
 
 	log.Info("[MAIN] Start homelock module:")
 	succ, err = redirect.StartRedirect(account, guid, config.Redirect)
-	log.Info("[MAIN] Homelock start stats:", succ)
+	log.Info("[MAIN] Homelock start stats:", succ, ", error:", err)
 	if false == succ {
-		log.Error("[MAIN] \t", err)
 		return
 	}
 
+	err = nil
 	log.Info("[MAIN] Module start end")
+}
+
+func main() {
+	var debug bool
+	var weight uint
+	var guid, account string
 
 	signal.Notify(common.ChanSignalExit, os.Interrupt, os.Kill)
 
+	flag.UintVar(&weight, "weight", 0, "program running weight")
+	flag.BoolVar(&debug, "debug", false, "Whether to start the debug mode")
+	flag.StringVar(&guid, "guid", "auto", "unique identifier, used to obtain user configuration")
+	flag.StringVar(&account, "account", "everyone", "user name, used to obtain user configuration")
+
+	flag.Parse()
+	logFile, err := initLogger("${APPDATA}\\SSOOR", "youniverse.log")
+	if nil != err {
+		log.Warning("open log file error:", err.Error())
+	}
+
+	defer logFile.Close()
+	defer log.Info("[MAIN] The Youniverse has finished running, exiting...")
+
+	go goRun(debug, weight, guid, account)
 	<-common.ChanSignalExit
 }
